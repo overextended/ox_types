839e0bff397c3e847e1a1a2cd79903e1a30449e5
ANTOND <71350868+antond15@users.noreply.github.com>
2024-03-01 01:49:33 +0100

    update inventory param types (#5)

--- types/ox_inventory/client.lua
+++ types/ox_inventory/client.lua
@@ -1,120 +1,160 @@
 ---@meta
 
----@return SlotWithItem?
+---**`client`**
+---@return SlotWithItem? currentWeapon
 function exports.ox_inventory:getCurrentWeapon() end
 
+---**`client`**
 ---@param id string
 ---@param owner? boolean | string | number
 function exports.ox_inventory:setStashTarget(id, owner) end
 
+---**`client`**
 ---@param type 'player'
 ---@param data? number
 function exports.ox_inventory:openInventory(type, data) end
 
+---**`client`**
 ---@param type 'policeevidence'
 ---@param data? number | string
 function exports.ox_inventory:openInventory(type, data) end
 
+---**`client`**
 ---@param type 'shop'
 ---@param data { id: number, type: string }
 function exports.ox_inventory:openInventory(type, data) end
 
+---**`client`**
 ---@param type 'stash'
 ---@param data string | { id: number, type?: string, owner?: boolean | string | number }
 function exports.ox_inventory:openInventory(type, data) end
 
+---**`client`**
 ---@param type 'dumpster'
 ---@param data string
 function exports.ox_inventory:openInventory(type, data) end
 
+---**`client`**
 ---@param type 'drop'
 ---@param data number
 function exports.ox_inventory:openInventory(type, data) end
 
+---**`client`**
 ---@param type 'glovebox' | 'trunk'
 ---@param data { id: string, netid: number }
 function exports.ox_inventory:openInventory(type, data) end
 
+---**`client`**
 ---@param type 'crafting'
 ---@param data { id: number, index: number }
 function exports.ox_inventory:openInventory(type, data) end
 
+---**`client`**
 ---@param type 'container'
 ---@param data number
 function exports.ox_inventory:openInventory(type, data) end
 
+---**`client`**
 ---@param data SlotWithItem
----@param cb fun(data: SlotWithItem)
+---@param cb? fun(data: SlotWithItem | false)
 function exports.ox_inventory:useItem(data, cb) end
 
+---**`client`**
 ---@param slotId number
+---@return boolean? success
 function exports.ox_inventory:useSlot(slotId) end
 
+---**`client`**
 function exports.ox_inventory:openNearbyInventory() end
 
+---**`client`**
 function exports.ox_inventory:closeInventory() end
 
+---**`client`**
 ---@param serverId number
 ---@param slotId number
 ---@param count? number
 function exports.ox_inventory:giveItemToTarget(serverId, slotId, count) end
 
----@param search 'slots' | 'count'
+---**`client`**
+---@param search 'count'
 ---@param item string | string[]
----@param metadata? string | table<string, any>
+---@param metadata? metadata
+---@return number | table<string, number> count
 function exports.ox_inventory:Search(search, item, metadata) end
 
----@return table<number, SlotWithItem>
+---**`client`**
+---@param search 'slots'
+---@param item string | string[]
+---@param metadata? metadata
+---@return SlotWithItem | SlotWithItem[] items
+function exports.ox_inventory:Search(search, item, metadata) end
+
+---**`client`**
+---@return table<number, SlotWithItem?> items
 function exports.ox_inventory:GetPlayerItems() end
 
----@return number
+---**`client`**
+---@return number totalWeight
 function exports.ox_inventory:GetPlayerWeight() end
 
----@return number
+---**`client`**
+---@return number maxWeight
 function exports.ox_inventory:GetPlayerMaxWeight() end
 
+---**`client`**
 ---@param itemName string
----@param metadata? string | table<string, any>
----@param strict? boolean
----@return SlotWithItem?
+---@param metadata? metadata
+---@param strict? boolean strictly match metadata properties, otherwise use partial matching
+---@return SlotWithItem? item
 function exports.ox_inventory:GetSlotWithItem(itemName, metadata, strict) end
 
+---**`client`**
 ---@param itemName string
----@param metadata? string | table<string, any>
----@param strict? boolean
----@return number?
+---@param metadata? metadata
+---@param strict? boolean strictly match metadata properties, otherwise use partial matching
+---@return number? slotId
 function exports.ox_inventory:GetSlotIdWithItem(itemName, metadata, strict) end
 
+---**`client`**
 ---@param itemName string
----@param metadata? string | table<string, any>
----@param strict? boolean
----@return SlotWithItem[]?
+---@param metadata? metadata
+---@param strict? boolean strictly match metadata properties, otherwise use partial matching
+---@return SlotWithItem[]? items
 function exports.ox_inventory:GetSlotsWithItem(itemName, metadata, strict) end
 
+---**`client`**
 ---@param itemName string
----@param metadata? string | table<string, any>
----@param strict? boolean
----@return number[]?
+---@param metadata? metadata
+---@param strict? boolean strictly match metadata properties, otherwise use partial matching
+---@return number[]? slotIds
 function exports.ox_inventory:GetSlotIdsWithItem(itemName, metadata, strict) end
 
+---**`client`**
 ---@param itemName string
----@param metadata? string | table<string, any>
----@param strict? boolean
----@return number
+---@param metadata? metadata
+---@param strict? boolean strictly match metadata properties, otherwise use partial matching
+---@return number count
 function exports.ox_inventory:GetItemCount(itemName, metadata, strict) end
 
+---**`client`**
 ---@param metadata table<string, string> | table<string, string>[]
 function exports.ox_inventory:displayMetadata(metadata) end
 
+---**`client`**
 ---@param metadata string
 ---@param value string
 function exports.ox_inventory:displayMetadata(metadata, value) end
 
----@return table<string, OxClientItem>
+---**`client`**
+---@return table<string, OxClientItem> items
 function exports.ox_inventory:Items() end
 
----@return OxClientItem?
+---**`client`**
+---@param itemName string
+---@return OxClientItem? item
 function exports.ox_inventory:Items(itemName) end
 
----@param state boolean?
+---**`client`**
+---@param state? boolean
 function exports.ox_inventory:weaponWheel(state) end
--- types/ox_inventory/server.lua
+++ types/ox_inventory/server.lua
@@ -41,226 +41,286 @@
 ---@field coords? vector3
 ---@field items? { [number]: string, [number]: number, [number]: table | string }[]
 
----@param player { source: number, name?: string, groups?: table, dateofbirth?: string, sex?: string }
----@param minimalInventory { name: string, slot: number, count: number, metadata?: table }[]?
+---**`server`**
+---@param player { source: number, identifier: string | number, name: string, groups?: table, dateofbirth?: string, sex?: string }
+---@param minimalInventory? { name: string, slot: number, count: number, metadata?: table }[]
 function exports.ox_inventory:setPlayerInventory(player, minimalInventory) end
 
+---**`server`**
 ---@param playerId number
 ---@param invType string
----@param data string | number | table
+---@param data? string | number | table
 function exports.ox_inventory:forceOpenInventory(playerId, invType, data) end
 
+---**`server`**
 ---@param playerId number
 ---@param items table
----@return table<number, SlotWithItem>, number
+---@return table<number, SlotWithItem> items updated item data
+---@return number totalWeight
 function exports.ox_inventory:ConvertItems(playerId, items) end
 
+---**`server`**
 ---@param event string
 ---@param cb function
 ---@param options? table
 ---@return number
 function exports.ox_inventory:registerHook(event, cb, options) end
 
+---**`server`**
 ---@param id? number
 function exports.ox_inventory:removeHooks(id) end
 
+---**`server`**
 ---@param inv inventory
 ---@param owner? string | number
 ---@return OxInventory?
 function exports.ox_inventory:GetInventory(inv, owner) end
 
+---**`server`**
 ---@param inv inventory
 ---@param owner? string | number
 ---@return table<number, SlotWithItem>?
 function exports.ox_inventory:GetInventoryItems(inv, owner) end
 
+---**`server`**
 ---@param inv inventory
 ---@param slotId number
 ---@return OxInventory?
 function exports.ox_inventory:GetContainerFromSlot(inv, slotId) end
 
+---**`server`**
 ---@param inv inventory
 ---@param item string
----@param metadata? string | table<string, any>
+---@param metadata? metadata
 ---@param returnsCount? boolean
 ---@return OxServerItem | number
 function exports.ox_inventory:GetItem(inv, item, metadata, returnsCount) end
 
+---**`server`**
 ---@param inv inventory
 ---@param item string
 ---@param count number
----@param metadata? string | table<string, any>
----@return boolean, nil | string | SlotWithItem | SlotWithItem[]
+---@param metadata? metadata
+---@return boolean success
+---@return nil | string | SlotWithItem | SlotWithItem[] response
 function exports.ox_inventory:SetItem(inv, item, count, metadata) end
 
+---**`server`**
 ---@param inv inventory
 ---@return SlotWithItem?
 function exports.ox_inventory:GetCurrentWeapon(inv) end
 
+---**`server`**
 ---@param inv inventory
 ---@param slotId number
 ---@return SlotWithItem?
 function exports.ox_inventory:GetSlot(inv, slotId) end
 
+---**`server`**
 ---@param inv inventory
 ---@param slotId number
 ---@param durability? number
 function exports.ox_inventory:SetDurability(inv, slotId, durability) end
 
+---**`server`**
 ---@param inv inventory
 ---@param slotId number
----@param metadata table<string, any>
+---@param metadata metadata
 function exports.ox_inventory:SetMetadata(inv, slotId, metadata) end
 
+---**`server`**
 ---@param inv inventory
 ---@param slots number
 function exports.ox_inventory:SetSlotCount(inv, slots) end
 
+---**`server`**
 ---@param inv inventory
 ---@param maxWeight number
 function exports.ox_inventory:SetMaxWeight(inv, maxWeight) end
 
+---**`server`**
 ---@param inv inventory
 ---@param item string
 ---@param count number
----@param metadata? string | table<string, any>
+---@param metadata? metadata
 ---@param slot? number
 ---@param cb? fun(success: boolean, response?: string) deprecated
----@return boolean, nil | string | SlotWithItem | SlotWithItem[]
+---@return boolean success
+---@return nil | string | SlotWithItem | SlotWithItem[] response
 function exports.ox_inventory:AddItem(inv, item, count, metadata, slot, cb) end
 
+---**`server`**
 ---@param inv inventory
----@param search 'slots' | 'count'
+---@param search 'slots'
 ---@param item string | string[]
----@param metadata? string | table<string, any>
+---@param metadata? metadata
+---@return SlotWithItem | SlotWithItem[] items
 function exports.ox_inventory:Search(inv, search, item, metadata) end
 
+---**`server`**
+---@param inv inventory
+---@param search 'count'
+---@param item string | string[]
+---@param metadata? metadata
+---@return number | table<string, number> count
+function exports.ox_inventory:Search(inv, search, item, metadata) end
+
+---**`server`**
 ---@param inv inventory
 ---@param item string
----@param metadata? table
+---@param metadata? metadata
+---@return table<number, number> slots `[slotId]: count`
+---@return number totalCount
+---@return number emptySlots amount of remaining empty slots
 function exports.ox_inventory:GetItemSlots(inv, item, metadata) end
 
+---**`server`**
 ---@param inv inventory
 ---@param item string
 ---@param count number
----@param metadata? string | table<string, any>
+---@param metadata? metadata
 ---@param slot? number
 ---@param ignoreTotal? boolean
----@return boolean?, string?
+---@return boolean success
+---@return string? response
 function exports.ox_inventory:RemoveItem(inv, item, count, metadata, slot, ignoreTotal) end
 
+---**`server`**
 ---@param inv inventory
 ---@param item string
 ---@param count number
----@param metadata? string | table<string, any>
----@return boolean?
+---@param metadata? metadata
+---@return boolean? canCarry
 function exports.ox_inventory:CanCarryItem(inv, item, count, metadata) end
 
+---**`server`**
 ---@param inv inventory
 ---@param item string
----@return number?
+---@return number? amount
 function exports.ox_inventory:CanCarryAmount(inv, item) end
 
+---**`server`**
 ---@param inv inventory
 ---@param weight number
----@return boolean?, number?
+---@return boolean? canHold
+---@return number? availableWeight
 function exports.ox_inventory:CanCarryWeight(inv, weight) end
 
+---**`server`**
 ---@param inv inventory
 ---@param firstItem string
 ---@param firstItemCount number
 ---@param testItem string
 ---@param testItemCount number
----@return boolean?
+---@return boolean? canSwap
 function exports.ox_inventory:CanSwapItem(inv, firstItem, firstItemCount, testItem, testItemCount) end
 
+---**`server`**
 ---@param prefix string
----@param items { [1]: string, [2]: number, [3]: string | table<string, any> }[]
+---@param items { [1]: string, [2]: number, [3]: metadata? }[] `{ name, count, metadata }`
 ---@param coords vector3
----@param slots number
----@param maxWeight number
----@param instance number | string
+---@param slots? number
+---@param maxWeight? number
+---@param instance? string | number
 ---@param model? string | number
----@return string
+---@return string dropId
 function exports.ox_inventory:CustomDrop(prefix, items, coords, slots, maxWeight, instance, model) end
 
+---**`server`**
 ---@param playerId number
----@return string
+---@return string dropId
 function exports.ox_inventory:CreateDropFromPlayer(playerId) end
 
+---**`server`**
 ---@param playerId number
 function exports.ox_inventory:ConfiscateInventory(playerId) end
 
+---**`server`**
 ---@param playerId number
 function exports.ox_inventory:ReturnInventory(playerId) end
 
----@param playerId number
----@param keep? string | string[]
-function exports.ox_inventory:ClearInventory(playerId, keep) end
-
+---**`server`**
 ---@param inv inventory
----@return number?
+---@param keep? string | string[] item or list of items to ignore while clearing items
+function exports.ox_inventory:ClearInventory(inv, keep) end
+
+---**`server`**
+---@param inv inventory
+---@return number? slotId
 function exports.ox_inventory:GetEmptySlot(inv) end
 
+---**`server`**
 ---@param inv inventory
 ---@param itemName string
----@param metadata? string | table<string, any>
----@return number?
+---@param metadata? metadata
+---@return number? slotId
 function exports.ox_inventory:GetSlotForItem(inv, itemName, metadata) end
 
+---**`server`**
 ---@param inv inventory
 ---@param itemName string
----@param metadata? string | table<string, any>
----@param strict? boolean
----@return SlotWithItem?
+---@param metadata? metadata
+---@param strict? boolean strictly match metadata properties, otherwise use partial matching
+---@return SlotWithItem? item
 function exports.ox_inventory:GetSlotWithItem(inv, itemName, metadata, strict) end
 
+---**`server`**
 ---@param inv inventory
 ---@param itemName string
----@param metadata? string | table<string, any>
----@param strict? boolean
----@return number?
+---@param metadata? metadata
+---@param strict? boolean strictly match metadata properties, otherwise use partial matching
+---@return number? slotId
 function exports.ox_inventory:GetSlotIdWithItem(inv, itemName, metadata, strict) end
 
+---**`server`**
 ---@param inv inventory
 ---@param itemName string
----@param metadata? string | table<string, any>
----@param strict? boolean
----@return SlotWithItem[]?
+---@param metadata? metadata
+---@param strict? boolean strictly match metadata properties, otherwise use partial matching
+---@return SlotWithItem[]? items
 function exports.ox_inventory:GetSlotsWithItem(inv, itemName, metadata, strict) end
 
+---**`server`**
 ---@param inv inventory
 ---@param itemName string
----@param metadata? string | table<string, any>
----@param strict? boolean
----@return number[]?
+---@param metadata? metadata
+---@param strict? boolean strictly match metadata properties, otherwise use partial matching
+---@return number[]? slotIds
 function exports.ox_inventory:GetSlotIdsWithItem(inv, itemName, metadata, strict) end
 
+---**`server`**
 ---@param inv inventory
 ---@param itemName string
----@param metadata? string | table<string, any>
----@param strict? boolean
----@return number
+---@param metadata? metadata
+---@param strict? boolean strictly match metadata properties, otherwise use partial matching
+---@return number count
 function exports.ox_inventory:GetItemCount(inv, itemName, metadata, strict) end
 
+---**`server`**
 ---@param name string
 ---@param label string
 ---@param slots number
 ---@param maxWeight number
----@param owner? string | number | boolean
+---@param owner? boolean | string | number
 ---@param groups? table<string, number>
----@param coords? vector3 | table<vector3>
+---@param coords? vector3 | vector3[]
 function exports.ox_inventory:RegisterStash(name, label, slots, maxWeight, owner, groups, coords) end
 
+---**`server`**
 ---@param properties TemporaryStashProperties
+---@return string stashId
 function exports.ox_inventory:CreateTemporaryStash(properties) end
 
----@return table<string, OxServerItem>
+---**`server`**
+---@return table<string, OxServerItem> items
 function exports.ox_inventory:Items() end
 
----@return OxServerItem?
+---**`server`**
+---@param itemName string
+---@return OxServerItem? item
 function exports.ox_inventory:Items(itemName) end
 
+---**`server`**
 ---@param archetype string
 ---@param data OxShopProperties
 function exports.ox_inventory:RegisterShop(archetype, data) end
