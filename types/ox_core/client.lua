0579eb5ee5149c1ce40752c7879b4724d0f2c3b9
FjamZoo <laurids112@live.dk>
2025-01-23 02:04:24 +0100

    Fix: remove player as an arg

--- types/ox_core/client.lua
+++ types/ox_core/client.lua
@@ -5,68 +5,57 @@
 player = {}
 
 ---**`client`**
----@param player OxPlayerClient
 ---@param statusName string
 ---@param value number
 ---@return boolean success
-function player.addStatus(player, statusName, value) end
+function player.addStatus(statusName, value) end
 
 ---**`client`**
----@param player OxPlayerClient
 ---@param key string
 ---@return any
-function player.get(player, key) end
+function player.get(key) end
 
 ---**`client`**
----@param player OxPlayerClient
 ---@return vector3
-function player.getCoords(player) end
+function player.getCoords() end
 
 ---**`client`**
----@param player OxPlayerClient
 ---@param filter string|string[]|{ [string]: number }
 ---@return any
-function player.getGroup(player, filter) end
+function player.getGroup(filter) end
 
 ---**`client`**
----@param player OxPlayerClient
 ---@param groupType string
 ---@return string groupName
 ---@return number grade
-function player.getGroupByType(player, groupType) end
+function player.getGroupByType(groupType) end
 
 ---**`client`**
----@param player OxPlayerClient
 ---@return table<string, number> groups
-function player.getGroups(player) end
+function player.getGroups() end
 
 ---**`client`**
----@param player OxPlayerClient
 ---@param statusName string
 ---@return number
-function player.getStatus(player, statusName) end
+function player.getStatus(statusName) end
 
 ---**`client`**
----@param player OxPlayerClient
 ---@return table<string, number> statuses
-function player.getStatuses(player) end
+function player.getStatuses() end
 
 ---**`client`**
----@param player OxPlayerClient
 ---@param permission string
 ---@return boolean
-function player.hasPermission(player, permission) end
+function player.hasPermission(permission) end
 
 ---**`client`**
----@param player OxPlayerClient
 ---@param statusName string
 ---@param value number
 ---@return boolean success
-function player.removeStatus(player, statusName, value) end
+function player.removeStatus(statusName, value) end
 
 ---**`client`**
----@param player OxPlayerClient
 ---@param key string
 ---@param cb fun(data: any) # callback receiving new value of `key`
 ---@return any data         # new value of `key`
-function player.on(player, key, cb) end
+function player.on(key, cb) end
